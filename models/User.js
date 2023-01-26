0b81400bdb7b505617923833c5e30f21ebdd586b
Shawn <shwnsmith12@gmail.com>
2023-01-26 10:08:00 -0600

    initial commit. some folders created, server.js file created. User and Thought models fleshed out

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require{'mongoose'};
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        // REGEX used to validate email
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+})
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User
